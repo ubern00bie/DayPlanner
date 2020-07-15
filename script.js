6d18d06076362072a79a2a5bdd9412a447088b99
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-07-15 14:13:27 -0500

    added time check and color setting functionality to main loop

--- script.js
+++ script.js
@@ -2,98 +2,91 @@ var currentDay = $('#currentDay').html();
     currentDay = moment().format(' M/D/YYYY');
 var currentTime = moment().format('H:HH A')
 var currentTimeDiv = $('<p id="currentTime">' + "Current Time: " + currentTime + '</p>;');
+var timeCheck = moment().format('H');
 
 $('#currentDay').append(currentDay);  
 $(".jumbotron").append(currentTimeDiv);
 
-var workDay = [
-    {
-    time:"8:00 AM",
-    task: "",
-    },
-    {
-    time:"9:00 AM",
-    task: "",
-    },
-    {
-    time:"10:00 AM",
-    task: "",
-    },
-    {
-    time:"11:00 AM",
-    task: "",
-    },
-    {
-    time:"12:00 PM",
-    task: "Lunch Time!",
-    },
-    {
-    time:"1:00 PM",
-    task: "",
-    },
-    {
-    time:"2:00 PM",
-    task: "",
-    },
-    {
-    time:"3:00 PM",
-    task: "",
-    },
-]
+var workDay = (JSON.parse(localStorage.getItem("workDay")) || [
+  {
+  time:"8:00 AM",
+  task: "",
+  timeValue:8
+  },
+  {
+  time:"9:00 AM",
+  task: "",
+  timeValue:9
+  },
+  {
+  time:"10:00 AM",
+  task: "",
+  timeValue:10
+  },
+  {
+  time:"11:00 AM",
+  task: "",
+  timeValue:11
+  },
+  {
+  time:"12:00 PM",
+  task: "Lunch Time!",
+  timeValue:12
+  },
+  {
+  time:"1:00 PM",
+  task: "",
+  timeValue:13
+  },
+  {
+  time:"2:00 PM",
+  task: "",
+  timeValue:14
+  },
+  {
+  time:"3:00 PM",
+  task: "",
+  timeValue:15
+  },]);
+$(workDay).each(function(index){ //loop to create time-blocks
 
-$(document).ready(function(){
-var workDay = JSON.parse(localStorage.getItem("workDay"));
-if (!localStorage.getItem('workDay')) {
-    taskUpdate(workDay);
-}
-else { 
-  workDay = new Array(8);
-  workDay[5] = "Lunch Time!";
-}
+ //set time for each timeblock
+  var timeBlock = document.createElement('div');
+      timeBlock.setAttribute('class','timeBlock'+index);
+      timeBlock.innerText = workDay[index].time;
+
+//set task for each text area
+  var textArea = document.createElement('textArea');
+      textArea.setAttribute('class','textArea')
+      textArea.innerText = workDay[index].task;
+
+//add a save button with an event listener
+  var save = document.createElement('button'); 
+      save.innerText = "save";
+      save.setAttribute('class','save');
+      save.setAttribute('id',index);
+      // save.addEventListener('click',saveTask);
+
+//appending elements
+      timeBlock.append(textArea);
+      timeBlock.append(save);
+      $('.container').append(timeBlock);
+//adjusting classes for past, present, future CSS 
+
+      if(this.timeValue < timeCheck) {
+        $(timeBlock).addClass("past-hour");
+      } else if (this.timeValue > timeCheck) {
+        $(timeBlock).addClass("future-hour");
+      } else {
+        $(timeBlock).addClass("present-hour");
+      }
 })
-var blockIndex = 0;
-for (var hour = 8; hour <= 16; hour++) {
-    // var index = hour - 8
-    var textArea = document.createElement('textArea');
-    textArea.setAttribute('class','timeBlock'+blockIndex)
-    console.log("outside if " + blockIndex)
-    $('textArea'[blockIndex]).text("");
-    console.log($('textArea'[blockIndex]));
-    $('textArea').text(workDay[blockIndex]);
-    var timeBlock = document.createElement('div');
-    timeBlock.setAttribute('class','workHour'+blockIndex);
-    var save = document.createElement('button'); 
-    save.innerText = "save";
-    save.setAttribute('class','save');
-    save.setAttribute('id',blockIndex);
-    save.addEventListener('click',saveTask);
+  $("button").click(function() {
+    value = $(this).siblings("textArea").val();
+    workDay[this.id].task = value;
+    localStorage.setItem("workDay", JSON.stringify(workDay))
+    
+  });
+ 
 
-    var blockHour;
-    if (hour === 12) { //sets time to AM or PM
-    blockHour = hour;
-    timeBlock.innerText = blockHour + "PM"; 
-    }
-    else if (hour>12) {
-    blockHour = hour - 12;
-    timeBlock.innerText = blockHour + "PM"; 
-    }
-    else {
-    blockHour = hour;
-    timeBlock.innerText = blockHour + "AM"; 
-    }
-   
-    timeBlock.append(textArea);
-    timeBlock.append(save);
-    $('.container').append(timeBlock);
 
-    blockIndex++;
-  }
-
-  function taskUpdate(timeblock) {
-    $('textArea').each(function(index){
-        $(this).text(workDay[index].task)
-    })
-  }
-  function saveTask () {
-    alert ('is workin')
-  }
